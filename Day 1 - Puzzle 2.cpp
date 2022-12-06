b5ad15273548a7e7cb769afd14d180fa63395347
Sahibjot Singh <73854578+stati30241@users.noreply.github.com>
2022-12-05 22:01:35 -0500

    Create Day 1 - Puzzle 2.cpp

---
+++ Day 1 - Puzzle 2.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int main() {
+	std::ifstream ifs{ "input.txt " };
+	std::vector<int> calories = { 0 };
+	for (std::string str = ""; std::getline(ifs, str); ) {
+		if (str == "") calories.push_back(0);
+		else {
+			int cal = std::stoi(str);
+			calories.back() += cal;
+		}
+	}
+
+	std::sort(calories.begin(), calories.end(), [](int a, int b) { return a > b; });
+	int max3 = calories.at(0) + calories.at(1) + calories.at(2);
+
+	std::cout << max3 << std::endl;
+
+	return 0;
+}
