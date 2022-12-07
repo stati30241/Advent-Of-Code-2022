3fa173e7a11e2212f16c28a61c7b50da39444892
Sahibjot Singh <73854578+stati30241@users.noreply.github.com>
2022-12-06 22:52:11 -0500

    Create Day 4 - Puzzle 1.cpp

---
+++ Day 4 - Puzzle 1.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+int main() {
+	std::ifstream ifs{ "input.txt" };
+	if (!ifs.is_open()) return -1;
+
+	int count = 0;
+	for (std::string str = ""; std::getline(ifs, str); ) {
+		// Replaces the seperators with spaces
+		std::replace(str.begin(), str.end(), ',', ' ');
+		std::replace(str.begin(), str.end(), '-', ' ');
+		
+		// Reads in the four values
+		std::istringstream ss{ str };
+		int a, b, c, d;
+		ss >> a >> b >> c >> d;
+
+		// Checks if one interval contains the other
+		if ((a <= c && b >= d) || (c <= a && d >= b)) count++;
+	}
+
+	std::cout << count << std::endl;
+
+	return 0;
+}
